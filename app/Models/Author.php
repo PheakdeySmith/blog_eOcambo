e2566950dff01339d4673bbc81e6d456dd6bc925
pheakdeysmith <pheakdeysmith@gmail.com>
2025-02-19 11:30:01 +0700

    Update on author controller

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Author extends Model {
     use HasFactory;
 
-    protected $fillable = ['name', 'user_id'];
+    protected $fillable = ['name', 'user_id', 'image'];
 
     public function user() {
         return $this->belongsTo(User::class);
